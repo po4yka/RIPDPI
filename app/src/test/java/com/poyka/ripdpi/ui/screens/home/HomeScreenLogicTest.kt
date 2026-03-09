c0e72763f040a7a0b63ecafe6596c74833ab5bcd
Nikita Pochaev <pochaev.nik@gmail.com>
2026-03-09 16:58:55 +0400

    Defer notification permission until service start

--- app/src/test/java/com/poyka/ripdpi/ui/screens/home/HomeScreenLogicTest.kt
+++ app/src/test/java/com/poyka/ripdpi/ui/screens/home/HomeScreenLogicTest.kt
@@ -2,6 +2,7 @@ package com.poyka.ripdpi.ui.screens.home
 
 import com.poyka.ripdpi.activities.ConnectionState
 import com.poyka.ripdpi.activities.MainUiState
+import com.poyka.ripdpi.data.AppStatus
 import com.poyka.ripdpi.data.Mode
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
@@ -39,5 +40,38 @@ class HomeScreenLogicTest {
             )
 
         assertFalse(shouldOpenVpnPermission(uiState = uiState, vpnPermissionRequired = true))
+    }
+
+    @Test
+    fun `starts connection when disconnected and service is halted`() {
+        val uiState =
+            MainUiState(
+                appStatus = AppStatus.Halted,
+                connectionState = ConnectionState.Disconnected,
+            )
+
+        assertTrue(shouldStartConnection(uiState))
+    }
+
+    @Test
+    fun `does not treat connected state as a start request`() {
+        val uiState =
+            MainUiState(
+                appStatus = AppStatus.Running,
+                connectionState = ConnectionState.Connected,
+            )
+
+        assertFalse(shouldStartConnection(uiState))
+    }
+
+    @Test
+    fun `does not treat running service as a fresh start request`() {
+        val uiState =
+            MainUiState(
+                appStatus = AppStatus.Running,
+                connectionState = ConnectionState.Error,
+            )
+
+        assertFalse(shouldStartConnection(uiState))
     }
 }
