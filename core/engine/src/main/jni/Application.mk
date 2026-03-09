b42eef3e4632da72c6efd04f1f6282866f3c79f7
Nikita Pochaev <pochaev.nik@gmail.com>
2026-03-09 08:47:58 +0400

    refactor: extract core/engine module
    Move native proxy/VPN engine, C/JNI sources, and submodules
    into separate core/engine library.

--- core/engine/src/main/jni/Application.mk
+++ core/engine/src/main/jni/Application.mk
@@ -14,7 +14,7 @@
 #
 
 APP_OPTIM := release
-APP_PLATFORM := android-21
+APP_PLATFORM := android-27
 APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
 APP_CFLAGS := -O3 -DPKGNAME=com/poyka/ripdpi/core
 APP_CPPFLAGS := -O3 -std=c++11
