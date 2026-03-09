420be9b8167858499d738e55baafd8b4639318b0
Nikita Pochaev <pochaev.nik@gmail.com>
2026-03-09 08:47:47 +0400

    chore: upgrade Gradle wrapper and build plugins
    Update Gradle wrapper, properties, and build configuration
    as foundation for modularization and Compose migration.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.3.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.android.application") version "9.1.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.10" apply false
+    id("com.google.protobuf") version "0.9.4" apply false
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,9 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
@@ -15,3 +18,6 @@ dependencyResolutionManagement {
 
 rootProject.name = "RipDpi"
 include(":app")
+include(":core:data")
+include(":core:engine")
+include(":core:service")
