4c621ba0f2cc0eaeac6d9c0c6fcff38b29a19e92
Nikita Pochaev <pochaev.nik@gmail.com>
2026-03-17 07:26:02 +0400

    Improve Gradle caching and split diagnostics data

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -1,34 +1,24 @@
 import com.android.build.api.dsl.LibraryExtension
-import com.google.devtools.ksp.gradle.KspExtension
 
 plugins {
     id("ripdpi.android.library")
     id("ripdpi.android.hilt")
     id("ripdpi.android.protobuf")
     id("ripdpi.android.serialization")
-    alias(libs.plugins.ksp)
 }
 
 extensions.configure<LibraryExtension> {
     namespace = "com.poyka.ripdpi.core.data"
 }
 
-extensions.configure<KspExtension> {
-    arg("room.schemaLocation", "$projectDir/schemas")
-    arg("room.generateKotlin", "true")
-}
-
 dependencies {
     api(libs.protobuf.javalite)
     implementation(libs.androidx.datastore)
-    implementation(libs.bundles.room)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.core.ktx)
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.logcat)
-    ksp(libs.androidx.room.compiler)
 
     testImplementation(libs.bundles.unit.test)
     testImplementation(libs.androidx.test.core.ktx)
-    testImplementation(libs.androidx.room.testing)
 }
