36cb2d0ac1b555606c1a2ca03cbfcebeb111a8e1
Nikita Pochaev <pochaev.nik@gmail.com>
2026-06-25 12:28:52 +0400

    chore(release): bump version to v0.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -116,8 +116,8 @@ val releaseStoreFilePath = localOrEnv("signing.storeFile", "RIPDPI_SIGNING_STORE
 val releaseStorePassword = localOrEnv("signing.storePassword", "RIPDPI_SIGNING_STORE_PASSWORD")
 val releaseKeyAlias = localOrEnv("signing.keyAlias", "RIPDPI_SIGNING_KEY_ALIAS")
 val releaseKeyPassword = localOrEnv("signing.keyPassword", "RIPDPI_SIGNING_KEY_PASSWORD")
-val ripdpiVersionCode = 10
-val ripdpiVersionName = "0.1.2"
+val ripdpiVersionCode = 11
+val ripdpiVersionName = "0.1.3"
 
 val forwardedInstrumentationArguments =
     listOf(
